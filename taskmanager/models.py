ecaad724bd7201362a5a48e6204ca496bcc10b1f
theconclusionofacts <37virs85@gmail.com>
2022-07-13 15:10:18 +0100

    models

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -4,15 +4,24 @@ from django.utils import timezone
 
 User = get_user_model()
 
+DeskType = [
+    (1,'Table'),
+    (2,'Board'),
+]
 class Desk(models.Model):
     title = models.CharField(max_length=233,verbose_name='Название доски')
     user = models.ForeignKey(User,on_delete=models.CASCADE,verbose_name='Пользователь')
+    type = models.IntegerField(choices=DeskType)
 
 class Column(models.Model):
     title = models.CharField(max_length=233,verbose_name='Название колонки')
     position = models.IntegerField(verbose_name='Позиция колонки')
+    card = models.ForeignKey('Card',on_delete=models.DO_NOTHING)
 
 class Card(models.Model):
     title = models.CharField(max_length=233,verbose_name='Название карточки')
     description = models.TextField(max_length=777,verbose_name='Описание')
+    position = models.IntegerField(verbose_name='Позиция карточки')
+    draft = models.BooleanField(default=True,verbose_name='Черновик')
     created_at = models.DateTimeField(auto_now_add=True,verbose_name='Когда создано')
+    updated_at = models.DateTimeField(auto_now=True,verbose_name='Отредактировано')
