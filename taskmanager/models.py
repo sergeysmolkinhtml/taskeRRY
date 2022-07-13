7c76b8e75ad540cecf165605c35ec4bb4315d58a
theconclusionofacts <37virs85@gmail.com>
2022-07-13 16:50:15 +0100

    models

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -13,10 +13,11 @@ class Desk(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE,verbose_name='Пользователь')
     type = models.IntegerField(choices=DeskType)
 
+
 class Column(models.Model):
     title = models.CharField(max_length=233,verbose_name='Название колонки')
     position = models.IntegerField(verbose_name='Позиция колонки')
-    card = models.ForeignKey('Card',on_delete=models.DO_NOTHING)
+
 
 class Card(models.Model):
     title = models.CharField(max_length=233,verbose_name='Название карточки')
@@ -25,3 +26,5 @@ class Card(models.Model):
     draft = models.BooleanField(default=True,verbose_name='Черновик')
     created_at = models.DateTimeField(auto_now_add=True,verbose_name='Когда создано')
     updated_at = models.DateTimeField(auto_now=True,verbose_name='Отредактировано')
+    column = models.ForeignKey('Column',on_delete=models.DO_NOTHING,verbose_name='Колонка')
+    user_accepted_task = models.ForeignKey(User,on_delete=models.CASCADE,verbose_name='Кто взял таск')
